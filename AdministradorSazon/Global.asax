b1671e37a5bb63631851fbd0602f1deb542893e2
dbarriguete <dbarriguete@gmail.com>
2020-03-14 22:58:43 -0600

    Agregar archivos de proyecto.

---
+++ AdministradorSazon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdministradorSazon.MvcApplication" Language="C#" %>
---
+++ AdministradorSazon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdministradorSazon.Startup))]
+namespace AdministradorSazon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
